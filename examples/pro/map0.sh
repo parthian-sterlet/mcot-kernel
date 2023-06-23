efe2fad0447bfd9fdff51b1810283f072a5ee9e0
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2023-06-23 09:14:11 +0700

    Update map0.sh

--- examples/pro/map0.sh
+++ examples/pro/map0.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # You need to place "anchor_pro" exec file to this folder
-./anchor_pro GSM2827249_CREB1_hg38_4000.fasta creb1_49__pwm creb1_49__sga 22 30 GSM2827249_CREB1_hg38_pwm.dist GSM2827249_CREB1_hg38_sga.dist 0 50
+./anchor_pro GSM2827249_CREB1_hg38_4000.fasta creb1_49__pwm creb1_49__sga 22 30 GSM2827249_CREB1_hg38_pwm.dist GSM2827249_CREB1_hg38_sga.dist 0 50 0.0005 10 1.5
