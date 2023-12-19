28f0bb748260dc335df6f0013a8d1864f46f8be9
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2023-12-19 16:04:50 +0700

    Update build.sh

--- run/build.sh
+++ run/build.sh
@@ -1,3 +1,5 @@
+cd run
+
 chmod a+x command_line_one
 chmod a+x command_line_many
 chmod a+x command_line_pro
@@ -9,3 +11,22 @@ g++ -o mcot.exe ../src/anchor_vs_many_partners/mcot.cpp
 chmod a+x mcot_anchor_pro.exe
 chmod a+x mcot_anchor.exe
 chmod a+x mcot.exe
+
+cd ../include
+cat h12core_hg38.binary.tar.gz.part* > h12core_hg38.binary.tar.gz
+cat h12core_mm10.binary.tar.gz.part* > h12core_mm10.binary.tar.gz
+cat dapseq_at10.binary.tar.gz.part* > dapseq_at10.binary.tar.gz
+tar -xvzf h12core_hg38.binary.tar.gz
+tar -xvzf h12core_mm10.binary.tar.gz
+tar -xvzf dapseq_at10.binary.tar.gz
+
+cd ../genomes/at
+tar -xvzf ups1500.seq.tar.gz
+cd ../mm
+tar -xvzf ups2kb.seq.tar.gz
+cd ../hs
+tar -xvzf ups2kb.seq.tar.gz
+cd ..
+cd ..
+
+
