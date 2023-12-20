13d6a1a550fab19cbb44424d3db5c090af3d231a
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2023-12-20 12:20:39 +0700

    Update build.sh

--- run/build.sh
+++ run/build.sh
@@ -2,9 +2,9 @@ chmod a+x command_line_one
 chmod a+x command_line_many
 chmod a+x command_line_pro
 
-g++ -o mcot_anchor_pro.exe ../src/anchor_pro/mcot_anchor_pro.cpp
-g++ -o mcot_anchor.exe ../src/anchor_vs_one_partner/mcot_anchor.cpp
-g++ -o mcot.exe ../src/anchor_vs_many_partners/mcot.cpp
+g++ -o mcot_anchor_pro.exe ../src/mcot_anchor_pro.cpp
+g++ -o mcot_anchor.exe ../src/mcot_anchor.cpp
+g++ -o mcot.exe ../src/mcot.cpp
 
 chmod a+x mcot_anchor_pro.exe
 chmod a+x mcot_anchor.exe
