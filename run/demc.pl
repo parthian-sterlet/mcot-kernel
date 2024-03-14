0cefef40031cbe128898eea3a786c7cab7e6971a
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2024-03-14 09:53:06 +0700

    Update demc.pl

--- run/demc.pl
+++ run/demc.pl
@@ -1,7 +1,7 @@
-﻿#!/usr/bin/perl
+#!/usr/bin/perl
 use 5.8.1; use strict; use warnings;
 
-my ($cmd, $path_exe, $path_in, $path_out, $genome, $fasta);
+my ($cmd, $path_exe, $path_in, $path_out, $genome, $fasta, $binary);
 my ($motif_base, $motif_ext, $n_motifs, $pwm_ext, $dist_ext, $bin_ext, $pwm_log);
 my ($i, $spacer_min, $spacer_max, $genome_prom, $pvalue_ce, $log_pvalue_bonf, $asym_rat);
 
@@ -35,18 +35,22 @@ else{
    or die("Can't create directory \"$path_out\": $!\n");
 }
 
+$binary = $path_out . $motif_base;
+$binary = $binary . $bin_ext;
+unlink $binary;
+
 for($i=1;$i<=$n_motifs;$i++)
 {
 $cmd= "$path_exe/pfm_to_pwm/pfm_to_pwm ${path_in}${motif_base}${i}${motif_ext} $path_out/${motif_base}${i}${pwm_ext}";
 print "$cmd\n";
 system $cmd;
 
-$cmd= "$path_exe/pwm_thr_err/pwm_thr_err ${path_in}${motif_base}${i}${motif_ext} ${path_out}${motif_base}${i}${pwm_ext} ${genome_prom} ${path_out}${motif_base}${i}${dist_ext} ${path_out}${motif_base}${bin_ext} 0.002 0.0000005 ${pwm_log} 0.00002 0";
+$cmd= "$path_exe/pwm_thr_err/pwm_thr_err ${path_in}${motif_base}${i}${motif_ext} ${path_out}${motif_base}${i}${pwm_ext} ${genome_prom} ${path_out}${motif_base}${i}${dist_ext} ${binary} 0.002 0.0000005 ${pwm_log} 0.00002 0";
 print "$cmd\n";
 system $cmd;
 }
 
-$cmd= "$path_exe/denovo/denovo ${path_in}${fasta} ${path_out}${motif_base}${bin_ext} ${n_motifs} ${spacer_min} ${spacer_max} ${pvalue_ce} ${log_pvalue_bonf} ${asym_rat}";
+$cmd= "$path_exe/denovo/denovo ${path_in}${fasta} ${binary} ${n_motifs} ${spacer_min} ${spacer_max} ${pvalue_ce} ${log_pvalue_bonf} ${asym_rat}";
 print "$cmd\n";
 system $cmd;
 
