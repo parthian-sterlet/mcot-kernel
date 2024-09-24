c4ef378dc7a4619cba82e7866ea7fbc18bb9ba7b
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2024-09-24 16:11:35 +0700

    Update unpack.sh

--- run/unpack.sh
+++ run/unpack.sh
@@ -14,13 +14,13 @@ tar -xvzf h12core_mm10.binary.tar.gz
 tar -xvzf dapseq_at10.binary.tar.gz
 
 cd ../genomes/at
-tar -xvzf ups1500_at10.seq.tar.gz
+tar -xzvf ups1500_at10.seq.tar.gz
 cd ../dm
-tar -xvzf ups1500_dm6.seq.tar.gz
+tar -xzvf ups1500_dm6.seq.tar.gz
 cd ../mm
-tar -xvzf ups2kb_mm10.seq.tar.gz
+tar -xzvf ups2kb_mm10.seq.tar.gz
 cd ../hs
-tar -xvzf ups2kb_hg38.seq.tar.gz
+tar -xzvf ups2kb_hg38.seq.tar.gz
 cd ..
 
 
